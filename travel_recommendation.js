183213fcd133afaed8be637be821f0d5ceac26f9
Darrell Walker <walkerdarrell15@gmail.com>
2026-03-28 15:16:33 -0400

    updated js code to return json data

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,12 +1,48 @@
+const btn = document.querySelector("#search-btn");
+btn.disabled = true;
+
 async function loadData() {
     try {
         const response = await fetch("travel_recommendation_api.json");
-        const data = await response.json();
+         const travelData = await response.json();
 
-        console.log(data);
+        btn.disabled = false;
+        displayResults(travelData);
     } catch (error) {
         console.log('Error:', error);
     }
 }
 
+function displayResults(destinations) {
+    document.querySelector("#search-btn").addEventListener("click", () => {
+        const input = document.querySelector("input").value;
+        const keyword = input.toLowerCase();
+    
+        if (!destinations) {
+            console.log("Data not loaded yet");
+            return;
+        }
+    
+        if (keyword.includes("country")){
+            console.log(destinations.countries);
+        }else if (keyword.includes("beach")){
+            console.log(destinations.beaches);
+        }else if (keyword.includes("temple")){
+            console.log(destinations.temples);
+        }else{
+            console.log("no results");
+        }
+     });
+    
+
+    console.log("Here is the data:", destinations);
+  }  
+
 loadData();
+
+ //clear button logic
+ document.querySelector("#clear-btn").addEventListener("click", () => {
+    const input = document.querySelector("input");
+    input.value = "";
+ });
+
